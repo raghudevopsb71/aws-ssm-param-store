dbaa9a061b35987c1cae2705f38481c87fc67a71
Raghu K <raghuk.vit@gmail.com>
2023-04-25 09:02:13 +0530

    Update aws-ssm-param-store/

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@
 for var in $(echo $1 | sed -e 's/,/ /g') ; do
   var_name=$(echo $var | awk -F : '{print $1}')
   parameter=$(echo $var | awk -F : '{print $2}')
-  aws ssm get-parameter --name $parameter --with-decryption | jq '.Parameter.Value'
+  value=(aws ssm get-parameter --name $parameter --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g')
+  echo ${var_name}=${value}
 done
